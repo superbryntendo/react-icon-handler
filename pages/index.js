42e975de4627c87f5b7f865379597503309b646b
Bryn Jackson <superbryntendo@github.com>
2020-05-26 15:31:53 -0700

    another icon test

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 import Icon from '../components/icon'
 
-const list = ['', 'instagram', 'check-fill','check-path', 'archive']
+const list = ['', 'instagram', 'check-fill', 'check-path', 'archive']
 const sizes = [32, 24, 16, 12, 8]
 
 export default function Home() {
